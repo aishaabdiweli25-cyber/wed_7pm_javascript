594a98d9d6b984786c962f03e518304c1efd1feb
Aisha Abdiweli <aishaabdiweli25@gmail.com>
2025-11-30 18:42:37 +0300

    want to push ch5 to github

---
+++ ch5_let_var_const/script.js
@@ -0,0 +1,14 @@
+// Example with var
+let x = 10;
+console.log(x); // 10
+x =  20; // can be reassigned
+console.log(x); // 20
+
+    
+
+{
+    let x = 50;
+    console.log(x); //50
+    x = 60;         //can be reassigned
+    console.log(x); //60
+}
