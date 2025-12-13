6f38a664c4b9bf545ef0d9cb5597a42801636e97
Aisha Abdiweli <aishaabdiweli25@gmail.com>
2025-12-13 18:19:19 +0300

    Add ch7_strings

---
+++ ch7_strings/script.js
@@ -0,0 +1,15 @@
+var firstName = "Aisha,Abdiweli,Abdullahi";
+// let lastName = ""
+// let age = "5"
+// var isStudent = "true"
+// let numberOfBrothers = 5
+// var hasPassport = true
+
+// console.log(firstName.indexOf("A"));
+// console.log(firstName.charAt(2));
+// console.log(firstName[2]);
+// console.log(firstName.length)
+// console.log(firstName.toUpperCase())
+// console.log(firstName.toLowerCase())
+// console.log(firstName.trim())
+console.log(firstName.split(","));
