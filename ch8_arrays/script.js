8d5baa3489f28bd4f34172e9d1b9368fe5683030
Aisha Abdiweli <aishaabdiweli25@gmail.com>
2025-12-16 15:45:22 +0300

    Add ch8 arrays

---
+++ ch8_arrays/script.js
@@ -0,0 +1,30 @@
+let firstNames = ["John", "Ali", "suad", "Abdi", "Fatima"];
+let ages = [23, 45];
+// console.log(firstNames[0])
+// console.log(firstNames[1])
+// console.log(ages[1])
+// console.log(ages.length)
+
+// let magacaKowaad = firstNames[1]
+// console.log(magacaKowaad)
+// console.log(firstNames[1]);
+
+// firstNames.push("suad", "Abdi", "Fatima")
+
+// firstNames = ["John", "Ali", "suad", "Abdi", "Fatima"];
+
+// // firstNames.pop();
+// // firstNames.unshift("Amira");
+// // firstNames.shift();
+// firstNames.length = 2
+// console.log(firstNames);
+
+var students = ["Ayman", "Hidaya", "Bilal", "Roodo", "Ridwaan", "Mohamed"];
+
+var anotherSmallArray = students.slice(0, 3); // create a new array from students
+
+console.log(anotherSmallArray);
+// Output: ['Ayman', 'Hidaya', 'Bilal']
+
+students.reverse();
+console.log(students);
