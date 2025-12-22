b9b0e68e8315c92430e967f4de20e1bd367f26b5
Aisha Abdiweli <aishaabdiweli25@gmail.com>
2025-12-22 17:16:01 +0300

    add ch11_objects

---
+++ ch11_objects/object.js
@@ -0,0 +1,50 @@
+// let student1 = {
+//   firstName: "Farhiya",
+//   lastName: "Ali",
+//   age: 30,
+// };
+
+// let student2 = {
+//   firstName: "Mohamed",
+//   lastName: "Bilal",
+//   age: 20,
+// };
+
+// console.log(`the age is   ${student1.age}`)
+
+// let person = new Object()
+// person.firstName = "Ali"
+// person.age = 20
+
+// person.walk = function () {
+//     console.log("this person is talking")
+// }
+
+// person.talk()
+
+// let person = {};
+// console.log(person);
+// person["firstName"] = "Ahmed";
+// // person.firstName = "Ahmed"
+
+// console.log(person["firstName"])
+
+// let person = {
+//     firstName: "Maryan",
+//     age : 20
+// }
+
+// let { firstName } = person
+
+// console.log(person.firstName)
+
+// let person = {
+//   firstName: "Maryan",
+//   lastName: "Burhan",
+//   age: 20,
+//   isMarried: false,
+// };
+
+// for (let prop in person) {
+//   console.log(person[prop]);
+// }
