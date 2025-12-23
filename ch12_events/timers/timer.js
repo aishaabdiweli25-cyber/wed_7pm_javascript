d5281365256e4afce2ac71d49d334ce87d6190af
Aisha Abdiweli <aishaabdiweli25@gmail.com>
2025-12-24 02:42:00 +0300

    add ch12 events

---
+++ ch12_events/timers/timer.js
@@ -0,0 +1,32 @@
+// function printMyInfo() {
+//   console.log("hello");
+// //   console.log("world");
+// //   console.log("my");
+// //   console.log("name");
+// //   console.log("is");
+// //   console.log("ahmed");
+// //   console.log("i");
+// //   console.log("live");
+// //   console.log("in");
+// //   console.log("London");
+// }
+
+// // let timer = setTimeout(printMyInfo, 10000);
+
+// // setTimeout(function () {
+// //   clearTimeout(timer);
+// // },15000);
+
+// // let timer = setInterval(printMyInfo, 1000)
+
+// // setTimeout(stopTimer,10000)
+
+// // function stopTimer() {
+// //     clearInterval(timer);
+// // }
+
+// function pray(salatType, NumOfRaa, location) {
+//   console.log(`pray ${salatType} of ${NumOfRaa} in ${location}`);
+// }
+
+// setTimeout(pray, 1000, "Cisha", "4", "Mosque");
