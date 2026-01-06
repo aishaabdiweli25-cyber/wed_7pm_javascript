a40cf203069dea4a41a6833134b9ab72c2927706
Aisha Abdiweli <aishaabdiweli25@gmail.com>
2026-01-06 17:50:38 +0300

    add ch15_error_handling

---
+++ ch15_error_handling/error.js
@@ -0,0 +1,14 @@
+try {
+ let firstName = "john";
+  console.log("aisha");
+  console.log("ahmed");
+  console.log("sabir " + firstName.toUpperCase());
+  console.log("ayub");
+  console.log("hafsa");
+} catch (error) {
+  console.log(error.message);
+} finally {
+  console.log("finally block")
+}
+// throw new Error("cilad ban abuuray");
+ 
